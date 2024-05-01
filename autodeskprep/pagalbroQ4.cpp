816de4b2da256980ea329104cdcfca714a51be8d
Cibiyanna <pkcibiyanna@gmail.com>
2024-05-01 21:49:45 +0530

    given 2 array , and 2 query to perform

---
+++ autodeskprep/pagalbroQ4.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void updateB(vector<int> &B,unordered_map<int,int> freq , int idx , int newVal ){
+    if(freq.count(B[idx])){  // decrement count of old value (if exists)
+        freq[B[idx]]--;
+    
+    }
+    B[idx] = newVal;
+    freq[newVal]++;  // increment count of new value
+}
+
+int query(vector<int>& A , unordered_map<int,int>& freq,int target){
+    int pairCount = 0;
+    for(int i=0;i<A.size();i++){
+        if(freq.count(target-A[i])){
+            pairCount += freq[target-A[i]];
+        }
+    }
+}
+
+int main()
+{
+    vector<int> A = {1,1,2};
+    vector<int> B = {2,1,3};
+    unordered_map<int,int> freq;
+    for(int i=0;i<A.size();i++){
+        freq[B[i]]++;
+    }
+    // first query is to update B 
+    updateB(B,freq,1,4);
+
+    int target = 10;
+    int pairCount = query(A,freq,target);
+    cout << "Number of pairs that sum to " << target << ": " << pairCount << endl;
+}
